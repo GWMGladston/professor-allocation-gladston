6cca5ac26e8667eefbbad206bc62aa52315ce695
GWMGladston <gladstonmoraes@gmail.com>
2022-03-10 18:40:58 -0300

    testando comits

--- src/main/java/com/project/professor/allocation/ProfessorAllocationApplication.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationApplication.java
@@ -8,6 +8,8 @@ public class ProfessorAllocationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProfessorAllocationApplication.class, args);
+        
+        //testando comit
     }
 
 }
